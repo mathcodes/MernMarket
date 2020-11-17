3a15949fbe3639a1c420c4f29468e658a2327124
Jon Christie <fullstack11235@gmail.com>
2020-11-17 08:30:53 -0500

    overhaul

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
+  mongoUri: 'JonMongoDB:JonMongoDB@devconnection.xzy3k.mongodb.net/mernMarket?retryWrites=true&w=majority' ||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
